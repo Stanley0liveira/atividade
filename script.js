a655a847316277465968a99b03f1ae49eb3fc650
Stanley Oliveira <stanleypintodeoliveira@gmail.com>
2026-06-12 22:17:11 -0300

    atualização do site

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function toggleMensagem(){
    CONTADOR
 ========================= */
 
-const inicio = new Date("2026-01-07T00:00:00");
+const inicio = new Date("2026-02-07T00:00:00");
 
 function atualizarContador(){
 
